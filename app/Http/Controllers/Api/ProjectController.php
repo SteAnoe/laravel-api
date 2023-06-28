7659afffd49b838961136775f8a0af60b8f0a6b5
SteAnoe <ste.anoe@gmail.com>
2023-06-28 13:02:18 +0200

    apicontroller done

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::with('type','technologies')->get();
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
