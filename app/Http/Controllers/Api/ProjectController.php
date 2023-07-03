1740aa3c3201a61a711adb0e67f0097e951ad7a3
SteAnoe <ste.anoe@gmail.com>
2023-07-03 18:32:24 +0200

    finito

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,13 +8,38 @@ use App\Models\Admin\Project;
 
 class ProjectController extends Controller
 {
-    public function index(){
-        $projects = Project::with('type','technologies')->get();
-        return response()->json([
+    public function index(Request $request){
+        // if($request->has('type_id')){
+        //     $projects = Project::with('type','technologies')->where('type_id', $request->type_id)->paginate(3);
+        // }else{
+        //     $projects = Project::with('type','technologies')->paginate(3);
+        // }
+
+        
+
+        // return response()->json([
+        //     'success' => true,
+        //     'projects' => $projects
+        // ]);
+        $query = Project::with(['type','technologies']);
+        if ($request->has('type_id')){
+            $query->where('type_id', $request->type_id);
+        }
+        if($request->has('technologies_ids')){
+            $technologyIds = explode(',', $request->technologies_ids);
+            $query->whereHas('technologies', function($query) use ($technologyIds){
+                $query->whereIn('id', $technologyIds);
+            });
+        }
+            
+        $projects = $query->paginate(3);
+            return response()->json([
             'success' => true,
             'projects' => $projects
         ]);
     }
+
+
     public function show($slug){
         $project = Project::with('type','technologies')->where('slug',$slug)->first();
         if($project){
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\TechnologyController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,4 +22,6 @@ use App\Http\Controllers\Api\ProjectController;
 
 route::get('/projects', [ProjectController::class, 'index']);
 route::get('/projects/{slug}', [ProjectController::class, 'show']);
+route::get('/types', [TypeController::class, 'index']);
+route::get('/technologies', [TechnologyController::class, 'index']);
 
